a9583671aa22cc64484ce3155ccd40fa61b86601
joshua <krjoshua21@gmail.com>
2022-01-20 12:32:20 +0900

    01/20

--- mywork/src/index.ts
+++ mywork/src/index.ts
@@ -1,1 +1,20 @@
-console.log(123)
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import http from "http";
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+})
+
+
+async function main() {
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise<void>(resolve => httpServer.listen({ port: 5000 }, resolve))
+  console.log(`Server running at http://localhost:5000${server.graphqlPath}`)
+}
+
+main();
